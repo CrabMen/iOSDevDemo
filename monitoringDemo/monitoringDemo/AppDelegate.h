62e1092e688a97a125e26d33b7404825e7b9e481
DragonLi <DragonLi_52171@163.com>
2017-11-19 00:20:41 +0800

    monitoringDemo

---
+++ monitoringDemo/monitoringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  monitoringDemo
+//
+//  Created by DragonLi on 2017/11/17.
+//  Copyright © 2017年 XiTu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ monitoringDemo/monitoringDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  monitoringDemo
+//
+//  Created by DragonLi on 2017/11/17.
+//  Copyright © 2017年 XiTu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
