c85f9effd98a9dec1ac3a76f0bd97beb75b48c43
DragonLi <DragonLi_52171@163.com>
2018-01-17 16:02:03 +0800

    add static inline example

--- InterviewExampleCode/InterviewExampleCode/ViewController.m
+++ InterviewExampleCode/InterviewExampleCode/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LFLConstInlineHeader.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+//    LFLImagePlaceholder()
     
 //    [self dispatch_queue_tDemo];
 //    [self dispatch_semaphore_tDemo];
@@ -40,8 +43,6 @@
     NSLog(@"%zd", j);
 
 }
-
-
 
 /**
  为线程加锁
