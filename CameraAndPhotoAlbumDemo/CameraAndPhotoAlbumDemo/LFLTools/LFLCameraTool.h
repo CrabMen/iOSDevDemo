ab10caed7779c2c522c298f812a4b7b845b91160
DragonLi <DragonLi_52171@163.com>
2018-04-11 18:48:52 +0800

    CameraAndPhotoAlbumDemo frist part

---
+++ CameraAndPhotoAlbumDemo/CameraAndPhotoAlbumDemo/LFLTools/LFLCameraTool.h
@@ -0,0 +1,13 @@
+//
+//  LFLCameraTool.h
+//  CameraAndPhotoAlbumDemo
+//
+//  Created by DragonLi on 2018/4/11.
+//  Copyright © 2018年 DragonLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LFLCameraTool : NSObject
+
+@end
